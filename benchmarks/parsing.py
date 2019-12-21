c5435731fda2392019171490fe93949ec67aaeda
Tishka17 <tishka17@mail.ru>
2019-12-21 20:18:54 +0300

    pydantic parsing bench

--- benchmarks/parsing.py
+++ benchmarks/parsing.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from timeit import timeit
 
 from marshmallow import Schema, fields, post_load
+from pydantic import BaseModel, Field
 from typing import List
 
 from dataclass_factory import Factory, Schema as DSchema
@@ -12,6 +13,17 @@ class Todo:
     id: int
     title: str
     desc: str
+
+
+# pydantic
+class PydanticTodo(BaseModel):
+    id: int
+    title: str
+    desc: str= Field(None, alias='description')
+
+
+class TodoList(BaseModel):
+    __root__: List[PydanticTodo]
 
 
 # marshmallow
@@ -51,7 +63,12 @@ def do2():
     return todo_schema.load(todos)[0]
 
 
+def do3():
+    return TodoList.parse_obj(todos)
+
+
 assert do1() == do2()
 
 print("my   ", timeit("do()", globals={"do": do1}, number=100000))  # 1.1471970899983717
 print("marsh", timeit("do()", globals={"do": do2}, number=100000))  # 9.297098876999371
+print("pydan", timeit("do()", globals={"do": do3}, number=100000))  # 5.090179265000188
