1b2a9be4fa114a541dfa59e5d1bb8bfd07b2b77a
Tishka17 <tishka17@mail.ru>
2020-01-04 00:00:21 +0300

    fix compat typed dict

--- tests/test_typeddict.py
+++ tests/test_typeddict.py
@@ -1,64 +1,30 @@
-try:
-    from typing import TypedDict  # type: ignore
-except ImportError:
-    try:
-        from mypy_extensions import TypedDict  # type: ignore
-    except ImportError:
-        TypedDict = object
-
+import sys
+from typing import Any
 from unittest import TestCase
+
+from nose2.tools import params  # type: ignore
+from typing_extensions import TypedDict as CompatTypedDict
 
 from dataclass_factory import Factory
 
+TYPED_DICTS: Any = [CompatTypedDict]
+if sys.version_info >= (3, 8):
+    from typing import TypedDict as PyTypedDict
 
-class Book(TypedDict):
-    name: str
-    year: int
-
-
-class Author(TypedDict):
-    author_name: str
-    book: Book
-
-
-class Author2(TypedDict):
-    name: str
-    book: Book
+    TYPED_DICTS.append(PyTypedDict)
 
 
 class TestTypedDict(TestCase):
-    def test_load(self):
-        if TypedDict is not object:
-            factory = Factory()
-            data = {
-                "name": "hello",
-                "year": 1
-            }
-            expected = Book(name="hello", year=1)
-            self.assertEqual(expected, factory.load(data, Book))
-        else:
-            self.skipTest("TypedDict is unsupported")
+    @params(*TYPED_DICTS)
+    def test_load(self, typed_dict):
+        class Book(typed_dict):
+            name: str
+            year: int
 
-    def test_load2(self):
-        if TypedDict is not object:
-            factory = Factory()
-            data = {
-                "author_name": "nobody",
-                "book": {"name": "hello", "year": 1},
-            }
-            expected = Author(author_name="nobody", book=Book(name="hello", year=1))
-            self.assertEqual(expected, factory.load(data, Author))
-        else:
-            self.skipTest("TypedDict is unsupported")
-
-    def test_load3(self):
-        if TypedDict is not object:
-            factory = Factory()
-            data = {
-                "name": "nobody",
-                "book": {"name": "hello", "year": 1},
-            }
-            expected = Author2(name="nobody", book=Book(name="hello", year=1))
-            self.assertEqual(expected, factory.load(data, Author2))
-        else:
-            self.skipTest("TypedDict is unsupported")
+        factory = Factory()
+        data = {
+            "name": "hello",
+            "year": 1
+        }
+        expected = Book(name="hello", year=1)
+        self.assertEqual(expected, factory.load(data, Book))
