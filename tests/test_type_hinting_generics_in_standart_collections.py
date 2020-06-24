8e9597ee31b45579d12772bf39302195eda67fa1
Tishka17 <tishka17@mail.ru>
2020-06-24 20:12:29 +0300

    fix tests

--- tests/test_type_hinting_generics_in_standart_collections.py
+++ tests/test_type_hinting_generics_in_standart_collections.py
@@ -9,8 +9,6 @@ from dataclass_factory import Factory
 @dataclass
 class Model:
     name: str
-    list: list[int]
-    dict: dict[int, int]
 
 
 factory = Factory()
@@ -21,49 +19,23 @@ class TestTypeHintingGenericsInStandartCollections(TestCase):
     def test_dict(self):
         data = {
             "model": {
-                "name": "name",
-                "list": [1, 2, 3, 4, 5],
-                "dict": {
-                    1: 1,
-                    2: 2,
-                    3: 3,
-                    4: 4,
-                },
+                "name": "name1",
             },
             "model2": {
-                "name": "name",
-                "list": [1, 2, 3, 4, 5],
-                "dict": {
-                    1: 1,
-                    2: 2,
-                    3: 3,
-                    4: 4,
-                },
+                "name": "name2",
             },
         }
-        self.assertTrue(factory.load(data, dict[str, Model]))
+        expected = {"model": Model("name1"), "model2": Model("name2")}
+        self.assertEqual(expected, factory.load(data, dict[str, Model]))
 
     def test_list(self):
         data = [
             {
-                "name": "name",
-                "list": [1, 2, 3, 4, 5],
-                "dict": {
-                    1: 1,
-                    2: 2,
-                    3: 3,
-                    4: 4,
-                }
+                "name": "name1",
             },
             {
-                "name": "name",
-                "list": [1, 2, 3, 4, 5],
-                "dict": {
-                    1: 1,
-                    2: 2,
-                    3: 3,
-                    4: 4,
-                },
+                "name": "name2",
             },
         ]
-        self.assertTrue(factory.load(data, list[Model]))
+        expected =[Model("name1"),  Model("name2")]
+        self.assertEqual(expected, factory.load(data, list[Model]))
