88b25dd80bf3d0820431e9135fb2eb1dad314cc6
pavel <p917910@gmail.com>
2020-09-27 01:14:19 +0300

    remove FieldsFilter from AllFieldsGetter, add exception if schema.only_mapped is True and schema.only is not None, reduce the code complexity

--- dataclass_factory/fields.py
+++ dataclass_factory/fields.py
@@ -1,11 +1,10 @@
 import inspect
 from dataclasses import Field, MISSING, fields, dataclass
-from functools import partial
-from typing import Sequence, Any, Type, TypeVar, Callable, List, Dict, Union, Optional, cast
+from typing import Sequence, Any, Type, TypeVar, Callable, List, Dict, Union, cast
 
 from .generics import resolve_hints, resolve_init_hints
 from .naming import convert_name
-from .path_utils import CleanPath, CleanKey, ellipsis, replace_ellipsis
+from .path_utils import CleanPath, CleanKey, replace_ellipsis, Key, Path
 from .schema import Schema
 from .type_detection import is_generic_concrete
 
@@ -24,8 +23,6 @@ class FieldInfo(BaseFieldInfo):
     data_name: Union[CleanKey, CleanPath]
 
 
-# defaults
-
 def get_dataclass_default(field: Field) -> Any:
     # type ignore because of https://github.com/python/mypy/issues/6910
     if field.default_factory != MISSING:  # type: ignore
@@ -40,39 +37,33 @@ def get_func_default(parameter: inspect.Parameter) -> Any:
     return parameter.default
 
 
-# all fields
-FieldsFilter = Callable[[str], bool]
-
-
-def all_dataclass_fields(cls, fields_filter: FieldsFilter = None) -> List[BaseFieldInfo]:
+def all_dataclass_fields(cls) -> List[BaseFieldInfo]:
     if is_generic_concrete(cls):
         all_fields = fields(cls.__origin__)
     else:
         all_fields = fields(cls)
     hints = resolve_hints(cls)
+
     return [
         BaseFieldInfo(field_name=f.name, type=hints[f.name], default=get_dataclass_default(f))
-        for f in all_fields
-        if not fields_filter or fields_filter(f.name) and f.init
+        for f in all_fields if f.init
     ]
 
 
-def all_class_fields(cls, fields_filter: FieldsFilter = None) -> List[BaseFieldInfo]:
+def all_class_fields(cls) -> List[BaseFieldInfo]:
     all_fields = inspect.signature(cls.__init__).parameters
     hints = resolve_init_hints(cls)
     return [
         BaseFieldInfo(field_name=f.name, type=hints.get(f.name, Any), default=get_func_default(f))
         for f in all_fields.values()
-        if not fields_filter or fields_filter(f.name)
     ]
 
 
-def all_typeddict_fields(cls, fields_filter: FieldsFilter = None) -> List[BaseFieldInfo]:
+def all_typeddict_fields(cls) -> List[BaseFieldInfo]:
     all_fields = resolve_hints(cls)
     return [
         BaseFieldInfo(field_name=f, type=t, default=MISSING)
         for f, t in all_fields.items()
-        if not fields_filter or fields_filter(f)
     ]
 
 
@@ -89,7 +80,7 @@ def schema_fields_filter(schema: Schema, name: str):
     )
 
 
-AllFieldsGetter = Callable[[Any, FieldsFilter], List[BaseFieldInfo]]
+AllFieldsGetter = Callable[[Any], List[BaseFieldInfo]]
 
 
 def get_fields(
@@ -97,37 +88,57 @@ def get_fields(
     schema: Schema[T],
     class_: Type[T]
 ) -> Sequence[FieldInfo]:
-    partial_filter_func: FieldsFilter = partial(schema_fields_filter, schema)  # type: ignore
-    all_fields = all_fields_getter(class_, partial_filter_func)
-    only_mapped = schema.only_mapped and schema.only is None
-    if only_mapped:
+    all_fields = [
+        field for field in all_fields_getter(class_)
+        if schema_fields_filter(schema, field.field_name)
+    ]
+
+    if schema.only_mapped:
+        if schema.only is not None:
+            raise ValueError('`only_mapped` is True, and `only` is not None')
         if schema.name_mapping is None:
             raise ValueError("`name_mapping` is None, and `only_mapped` is True")
-        fields_dict: Dict[str, BaseFieldInfo] = {f.field_name: f for f in all_fields}
-        fields_info: List[FieldInfo] = []
-        for field_name, data_name in schema.name_mapping.items():
-            if isinstance(field_name, ellipsis):
-                raise ValueError("`name_mapping` contains `...`, and `only_mapped` is True")
-            if field_name not in fields_dict:
-                continue
-            fields_info.append(FieldInfo(
-                field_name=cast(str, field_name),
-                data_name=replace_ellipsis(field_name, data_name),
-                type=fields_dict[field_name].type,
-                default=fields_dict[field_name].default,
-            ))
-        return tuple(fields_info)
+        if ... in schema.name_mapping:
+            raise ValueError("`name_mapping` contains `...`, and `only_mapped` is True")
+
+        return get_only_mapped_fields(
+            schema.name_mapping,  # noqa
+            all_fields
+        )
+
     whitelisted_fields = set(schema.only or []) | set(schema.name_mapping or [])
     return tuple(
         FieldInfo(
             field_name=f.field_name,
-            data_name=convert_name(f.field_name, schema.name_style, schema.name_mapping,
-                                   schema.trim_trailing_underscore),
+            data_name=convert_name(
+                f.field_name, schema.name_style,
+                schema.name_mapping, schema.trim_trailing_underscore
+            ),
             type=f.type,
             default=f.default,
         )
         for f in all_fields
-        if (not schema.skip_internal) or (f.field_name in whitelisted_fields) or (not f.field_name.startswith("_"))
+        if (not schema.skip_internal) or
+        (f.field_name in whitelisted_fields) or
+        (not f.field_name.startswith("_"))
+    )
+
+
+def get_only_mapped_fields(
+    name_mapping: Dict[str, Union[Key, Path]],
+    all_fields: List[BaseFieldInfo]
+):
+    fields_dict: Dict[str, BaseFieldInfo] = {f.field_name: f for f in all_fields}
+
+    return tuple(
+        FieldInfo(
+            field_name=cast(str, field_name),
+            data_name=replace_ellipsis(field_name, data_name),
+            type=fields_dict[field_name].type,
+            default=fields_dict[field_name].default,
+        )
+        for field_name, data_name in name_mapping.items()
+        if field_name in fields_dict
     )
 
 
