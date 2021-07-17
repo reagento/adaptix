c5420c7fce4ce0ca429c30e906f52d86778c24f0
pavel <p917910@gmail.com>
2021-07-17 10:09:16 +0300

    add provider templates for input and output fields add fields provider for namedtuple

--- dataclass_factory_30/low_level/fields.py
+++ dataclass_factory_30/low_level/fields.py
@@ -1,8 +1,12 @@
+import inspect
+from abc import abstractmethod
 from dataclasses import dataclass
+from inspect import Signature
 from types import MappingProxyType
-from typing import Any, Callable, Union
+from typing import Any, Callable, Union, List
 
-from ..core import ProvisionCtx
+from ..core import ProvisionCtx, Provider, BaseFactory, provision_action, CannotProvide
+from ..type_tools import is_subclass_soft
 
 
 @dataclass(frozen=True)
@@ -21,6 +25,103 @@ Default = Union[None, DefaultValue, DefaultFactory]
 @dataclass(frozen=True)
 class FieldsProvisionCtx(ProvisionCtx):
     field_name: str
-    field_in_init: bool
     default: Default
     metadata: MappingProxyType
+
+
+class InputFieldsProvider(Provider[List[FieldsProvisionCtx]]):
+    @abstractmethod
+    @provision_action
+    def _provide_input_fields(
+        self,
+        factory: 'BaseFactory',
+        offset: int,
+        ctx: ProvisionCtx
+    ) -> List[FieldsProvisionCtx]:
+        pass
+
+
+class OutputFieldsProvider(Provider[List[FieldsProvisionCtx]]):
+    @abstractmethod
+    @provision_action
+    def _provide_output_fields(
+        self,
+        factory: 'BaseFactory',
+        offset: int,
+        ctx: ProvisionCtx
+    ) -> List[FieldsProvisionCtx]:
+        pass
+
+
+class DataclassFieldsProvider(InputFieldsProvider, OutputFieldsProvider):
+    def _provide_input_fields(
+        self,
+        factory: 'BaseFactory',
+        offset: int,
+        ctx: ProvisionCtx
+    ) -> List[FieldsProvisionCtx]:
+        pass
+
+    def _provide_output_fields(
+        self,
+        factory: 'BaseFactory',
+        offset: int,
+        ctx: ProvisionCtx
+    ) -> List[FieldsProvisionCtx]:
+        pass
+
+
+def get_func_fields_prov_ctx(func, slice_=slice(0, None)) -> List[FieldsProvisionCtx]:
+    params = list(
+        inspect.signature(func).parameters.values()
+    )[slice_]
+
+    return [
+        FieldsProvisionCtx(
+            type=(
+                Any
+                if param.annotation is Signature.empty
+                else param.annotation
+            ),
+            field_name=param.name,
+            default=(
+                None
+                if param.default is Signature.empty
+                else DefaultValue(param.default)
+            ),
+            metadata=MappingProxyType({}),
+        )
+        for param in params
+    ]
+
+
+NAMED_TUPLE_METHODS = {'_fields', '_field_defaults', '_make', '_replace', '_asdict'}
+
+
+class NamedTupleFieldsProvider(InputFieldsProvider, OutputFieldsProvider):
+    def _get_fields(self, tp: type) -> List[FieldsProvisionCtx]:
+        if not (
+            is_subclass_soft(tp, tuple)
+            and
+            NAMED_TUPLE_METHODS.issubset(vars(tp))
+        ):
+            raise CannotProvide
+
+        return get_func_fields_prov_ctx(tp.__new__, slice(1, None))
+
+    def _provide_input_fields(
+        self,
+        factory: 'BaseFactory',
+        offset: int,
+        ctx: ProvisionCtx
+    ) -> List[FieldsProvisionCtx]:
+        return self._get_fields(ctx.type)
+
+    def _provide_output_fields(
+        self,
+        factory: 'BaseFactory',
+        offset: int,
+        ctx: ProvisionCtx
+    ) -> List[FieldsProvisionCtx]:
+        return self._get_fields(ctx.type)
+
---
+++ tests_30/fields/test_namedtuple.py
@@ -0,0 +1,130 @@
+from collections import namedtuple
+from types import MappingProxyType
+from typing import Any, NamedTuple
+
+from dataclass_factory_30.low_level.fields import NamedTupleFieldsProvider, FieldsProvisionCtx, DefaultValue
+
+FooAB = namedtuple('FooAB', 'a b')
+FooBA = namedtuple('FooBA', 'b a')
+
+
+def test_order():
+    assert (
+        NamedTupleFieldsProvider()._get_fields(FooAB)
+        ==
+        [
+            FieldsProvisionCtx(
+                type=Any,
+                field_name='a',
+                default=None,
+                metadata=MappingProxyType({})
+            ),
+            FieldsProvisionCtx(
+                type=Any,
+                field_name='b',
+                default=None,
+                metadata=MappingProxyType({})
+            ),
+        ]
+    )
+
+    assert (
+        NamedTupleFieldsProvider()._get_fields(FooBA)
+        ==
+        [
+            FieldsProvisionCtx(
+                type=Any,
+                field_name='b',
+                default=None,
+                metadata=MappingProxyType({})
+            ),
+            FieldsProvisionCtx(
+                type=Any,
+                field_name='a',
+                default=None,
+                metadata=MappingProxyType({})
+            ),
+        ]
+    )
+
+
+def func():
+    return 0
+
+
+FooDefs = namedtuple('FooDefs', 'a b c', defaults=[0, func])
+
+
+def test_defaults():
+    assert (
+        NamedTupleFieldsProvider()._get_fields(FooDefs)
+        ==
+        [
+            FieldsProvisionCtx(
+                type=Any,
+                field_name='a',
+                default=None,
+                metadata=MappingProxyType({})
+            ),
+            FieldsProvisionCtx(
+                type=Any,
+                field_name='b',
+                default=DefaultValue(0),
+                metadata=MappingProxyType({})
+            ),
+            FieldsProvisionCtx(
+                type=Any,
+                field_name='c',
+                default=DefaultValue(func),
+                metadata=MappingProxyType({})
+            ),
+        ]
+    )
+
+
+BarA = NamedTuple('BarA', a=int, b=str)
+
+
+class BarB(NamedTuple):
+    a: int
+    b: str = 'abc'
+
+
+def test_hinted_namedtuple():
+    assert (
+        NamedTupleFieldsProvider()._get_fields(BarA)
+        ==
+        [
+            FieldsProvisionCtx(
+                type=int,
+                field_name='a',
+                default=None,
+                metadata=MappingProxyType({})
+            ),
+            FieldsProvisionCtx(
+                type=str,
+                field_name='b',
+                default=None,
+                metadata=MappingProxyType({})
+            ),
+        ]
+    )
+
+    assert (
+        NamedTupleFieldsProvider()._get_fields(BarB)
+        ==
+        [
+            FieldsProvisionCtx(
+                type=int,
+                field_name='a',
+                default=None,
+                metadata=MappingProxyType({})
+            ),
+            FieldsProvisionCtx(
+                type=str,
+                field_name='b',
+                default=DefaultValue('abc'),
+                metadata=MappingProxyType({})
+            ),
+        ]
+    )
