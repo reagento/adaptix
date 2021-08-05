551ee783ba56dffd121b6426cea58713e0f915b2
pavel <p917910@gmail.com>
2021-08-05 15:41:53 +0300

    add is_typed_dict_class, is_named_tuple_class and is_generic_class to __init__.py of type_tools

--- dataclass_factory_30/type_tools/__init__.py
+++ dataclass_factory_30/type_tools/__init__.py
@@ -1,3 +1,6 @@
-from .utils import strip_alias, get_args, is_subclass_soft, is_new_type, is_annotated
+from .utils import (
+    strip_alias, get_args, is_subclass_soft, is_new_type, is_annotated,
+    is_typed_dict_class, is_named_tuple_class, is_generic_class
+)
 from .normalize_type import normalize_type, NormType, NormTV
 
