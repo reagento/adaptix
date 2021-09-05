f0204770ee1c56b4801b099bca78b51c240e2034
pavel <p917910@gmail.com>
2021-09-05 12:14:04 +0300

    remove test of provider core functions due to refactoring fix test_factory.py

--- tests_30/core/test_factory.py
+++ tests_30/core/test_factory.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 
-from dataclass_factory_30.core import Provider
+from dataclass_factory_30.core import Provider, collect_class_full_recipe
 from dataclass_factory_30.high_level.factory import BuiltinFactory
 
 
@@ -49,32 +49,25 @@ class Gen7Factory(Gen6Factory):
 
 
 def test_full_recipe():
-    b_fac = BuiltinFactory()
-    b_recipe = b_fac._full_recipe()
+    b_recipe = collect_class_full_recipe(BuiltinFactory)
 
     g1_recipe = b_recipe.copy()
-    assert Gen1Factory()._full_recipe() == g1_recipe
+    assert collect_class_full_recipe(Gen1Factory) == g1_recipe
 
     g2_recipe = [SP(1)] + g1_recipe
-    assert Gen2Factory()._full_recipe() == g2_recipe
+    assert collect_class_full_recipe(Gen2Factory) == g2_recipe
 
     g3_recipe = [SP(2)] + g2_recipe
-    assert Gen3Factory()._full_recipe() == g3_recipe
+    assert collect_class_full_recipe(Gen3Factory) == g3_recipe
 
     g4_recipe = g3_recipe.copy()
-    assert Gen4Factory()._full_recipe() == g4_recipe
+    assert collect_class_full_recipe(Gen4Factory) == g4_recipe
 
     g5_recipe = [SP(3)] + g4_recipe
-    assert Gen5Factory()._full_recipe() == g5_recipe
+    assert collect_class_full_recipe(Gen5Factory) == g5_recipe
 
     g6_recipe = g5_recipe.copy()
-    assert Gen6Factory()._full_recipe() == g6_recipe
+    assert collect_class_full_recipe(Gen6Factory) == g6_recipe
 
     g7_recipe = [SP(4)] + g6_recipe
-    assert Gen7Factory()._full_recipe() == g7_recipe
-
-    assert (
-        Gen1Factory([SP(10)])._full_recipe()
-        ==
-        b_recipe + [SP(10)]
-    )
+    assert collect_class_full_recipe(Gen7Factory) == g7_recipe
