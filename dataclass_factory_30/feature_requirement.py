ac1abe31038ada7a5683a9388d9ecf5c063d3c45
pavel <p917910@gmail.com>
2021-10-17 19:32:34 +0300

    refactor feature_requirement.py speed up VersionRequirement

--- dataclass_factory_30/feature_requirement.py
+++ dataclass_factory_30/feature_requirement.py
@@ -2,8 +2,16 @@ import sys
 from typing import Tuple
 
 
+def _true():
+    return True
+
+
+def _false():
+    return False
+
+
 class VersionRequirement:
-    __slots__ = ('ver', 'is_meet')
+    __slots__ = ('ver', 'is_meet', '__bool__')
 
     @classmethod
     def make(cls, *ver: int):
@@ -12,9 +20,7 @@ class VersionRequirement:
     def __init__(self, ver: Tuple[int, ...]):
         self.ver = ver
         self.is_meet = sys.version_info >= ver
-
-    def __bool__(self):
-        return self.is_meet
+        self.__bool__ = _true if self.is_meet else _false
 
     def __call__(self, func):
         import pytest
@@ -26,10 +32,16 @@ class VersionRequirement:
             reason=f'Need Python >= {ver_str}'
         )(func)
 
+    def __repr__(self):
+        return f"<{type(self).__qualname__} ver={self.ver} is_meet={self.is_meet}>"
 
-has_protocol = VersionRequirement.make(3, 8)
-has_literal = VersionRequirement.make(3, 8)
-has_final = VersionRequirement.make(3, 8)
-has_typed_dict = VersionRequirement.make(3, 8)
 
-has_annotated = VersionRequirement.make(3, 9)
+has_py_38 = VersionRequirement.make(3, 8)
+has_py_39 = VersionRequirement.make(3, 9)
+
+has_protocol = has_py_38
+has_literal = has_py_38
+has_final = has_py_38
+has_typed_dict = has_py_38
+
+has_annotated = has_py_39
