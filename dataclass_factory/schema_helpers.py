25d49d271e42ca6b9c78f97c83ab6e840230f124
Tishka17 <tishka17@mail.ru>
2021-11-09 20:51:06 +0300

    fix some hints

--- dataclass_factory/schema_helpers.py
+++ dataclass_factory/schema_helpers.py
@@ -6,8 +6,9 @@ from ipaddress import (
     IPv4Network, IPv6Network,
     IPv4Interface, IPv6Interface,
 )
+from operator import attrgetter
 from pathlib import Path
-from typing import Type, Dict, Any
+from typing import Type, Dict, Any, Generic, cast, Callable
 from uuid import UUID
 
 from .common import T, AbstractFactory, Parser
@@ -15,19 +16,19 @@ from .schema import Schema
 
 COMMON_SCHEMAS: Dict[Type, Schema] = {}
 try:
-    isodatetime_schema = Schema(
+    isodatetime_schema = Schema[datetime](
         parser=datetime.fromisoformat,  # type: ignore
         serializer=datetime.isoformat,
     )
     COMMON_SCHEMAS[datetime] = isodatetime_schema
 
-    isodate_schema = Schema(
+    isodate_schema = Schema[date](
         parser=date.fromisoformat,
         serializer=date.isoformat
     )
     COMMON_SCHEMAS[date] = isodate_schema
 
-    isotime_schema = Schema(
+    isotime_schema = Schema[time](
         parser=time.fromisoformat,
         serializer=time.isoformat
     )
@@ -35,9 +36,9 @@ try:
 except AttributeError:
     pass
 
-timedelta_schema = Schema(
+timedelta_schema = Schema[timedelta](
     parser=lambda x: timedelta(seconds=x),
-    serializer=lambda x: x.seconds
+    serializer=attrgetter("seconds"),
 )
 COMMON_SCHEMAS[timedelta] = timedelta_schema
 
@@ -49,15 +50,15 @@ def _parse_decimal(value: Any) -> decimal.Decimal:
         raise ValueError from e
 
 
-decimal_schema = Schema(
+decimal_schema = Schema[decimal.Decimal](
     parser=_parse_decimal,
     serializer=lambda x: format(x, "f"),
 )
 COMMON_SCHEMAS[decimal.Decimal] = decimal_schema
 
 
-def type_checker(value, field="type", pre_parse=None):
-    def check_type(data):
+def type_checker(value: Any, field="type", pre_parse=None) -> Callable:
+    def check_type(data: Any) -> Any:
         if value != data[field]:
             raise ValueError
         if pre_parse:
@@ -79,9 +80,8 @@ for type_ in (
     IPv4Interface, IPv6Interface,
 ):
     COMMON_SCHEMAS[type_] = Schema(
-        serializer=type_.__str__,
+        serializer=type_.__str__,  # type: ignore
         parser=type_,
-
     )
 
 
@@ -95,14 +95,16 @@ stub_schema = Schema(
 )
 
 
-class ClsCheckSchema(Schema[T]):
+class ClsCheckSchema(Schema[T], Generic[T]):
     serializer = _stub
 
-    def get_parser(self,
-                   cls: Type[T],
-                   stacked_factory: AbstractFactory,
-                   debug_path: bool) -> Parser[T]:
-        def cls_check_parser(data):
+    def get_parser(  # type: ignore
+        self,
+        cls: Type[T],
+        stacked_factory: AbstractFactory,
+        debug_path: bool,
+    ) -> Parser[T]:
+        def cls_check_parser(data: Any) -> T:
             if isinstance(data, cls):
                 return data
             raise TypeError(f'Argument must be {cls.__name__}')
