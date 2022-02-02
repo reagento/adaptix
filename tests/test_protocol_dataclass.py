4d5ed842fee05a3b4ad3e8562047766a9ddd6f15
Tishka17 <tishka17@mail.ru>
2022-02-02 14:36:29 +0300

    fix protocol on 3.7

--- tests/test_protocol_dataclass.py
+++ tests/test_protocol_dataclass.py
@@ -1,22 +1,24 @@
 from dataclasses import dataclass
-from typing import Protocol
+
+from typing_extensions import Protocol
+# typing_extensions just propagates typing.Protocol on python 3.8+
 
 from dataclass_factory import Factory
 
 
-class P(Protocol):
+class MyProto(Protocol):
     pass
 
 
 @dataclass
-class D(P):
+class Data(MyProto):
     field: int
 
 
 def test_protocol_parsed():
     factory = Factory()
     data = {"field": 1}
-    parsed = D(1)
+    parsed = Data(1)
 
-    assert factory.load(data, D) == parsed
+    assert factory.load(data, Data) == parsed
     assert factory.dump(parsed) == data
