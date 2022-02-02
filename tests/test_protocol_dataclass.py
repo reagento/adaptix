f5813dc1578351652090226b8edefc6510404f29
Tishka17 <tishka17@mail.ru>
2022-02-02 17:34:27 +0300

    fix generic protocols

--- tests/test_protocol_dataclass.py
+++ tests/test_protocol_dataclass.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
+from typing import TypeVar
 
-from typing_extensions import Protocol
 # typing_extensions just propagates typing.Protocol on python 3.8+
+from typing_extensions import Protocol
 
 from dataclass_factory import Factory
 
@@ -15,10 +16,45 @@ class Data(MyProto):
     field: int
 
 
+T = TypeVar("T")
+
+
+class MyGenericProto(Protocol[T]):
+    pass
+
+
+@dataclass
+class IntGenericData(MyGenericProto[int]):
+    field: int
+
+
+@dataclass
+class AnyGenericData(MyGenericProto):
+    field: int
+
+
 def test_protocol_parsed():
     factory = Factory()
     data = {"field": 1}
     parsed = Data(1)
 
     assert factory.load(data, Data) == parsed
+    assert factory.dump(parsed) == data
+
+
+def test_generic_parsed():
+    factory = Factory()
+    data = {"field": 1}
+    parsed = IntGenericData(1)
+
+    assert factory.load(data, IntGenericData) == parsed
+    assert factory.dump(parsed) == data
+
+
+def test_any_generic_parsed():
+    factory = Factory()
+    data = {"field": 1}
+    parsed = AnyGenericData(1)
+
+    assert factory.load(data, AnyGenericData) == parsed
     assert factory.dump(parsed) == data
