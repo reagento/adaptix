beb0a7fcc2952fced7bd9eb1ba0931eb0d0df8d6
pavel <p917910@gmail.com>
2022-02-03 21:54:51 +0300

    remove Json type hint from common.py remove compat.py

--- dataclass_factory_30/common.py
+++ dataclass_factory_30/common.py
@@ -1,17 +1,9 @@
-from typing import TypeVar, Union, Dict, List, Any, Callable
+from typing import TypeVar, Any, Callable
 
 K_contra = TypeVar('K_contra', contravariant=True)
 V_co = TypeVar('V_co', covariant=True)
 
 Parser = Callable[[Any], V_co]
 Serializer = Callable[[K_contra], Any]
-
-_JsonAtomic = Union[str, int, float, bool, None]
-_JsonBasic = Union[_JsonAtomic, list, Dict[str, Any]]
-Json = Union[
-    Dict[str, _JsonBasic],
-    List[_JsonBasic],
-    _JsonAtomic
-]
 
 TypeHint = Any
--- dataclass_factory_30/compat.py
+++
@@ -1,7 +0,0 @@
-try:
-    from typing import NewType
-except ImportError:
-    try:
-        from typing_extensions import NewType
-    except ImportError:
-        pass
