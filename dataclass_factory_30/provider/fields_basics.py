f008d43f4a1ddf4b1f74b302244b0782d8f3fd16
pavel <p917910@gmail.com>
2022-02-08 21:29:01 +0300

    extend comment of Crown, move it to module doc string

--- dataclass_factory_30/provider/fields_basics.py
+++ dataclass_factory_30/provider/fields_basics.py
@@ -1,3 +1,62 @@
+"""This module contains essential concepts of fields.
+
+Crown defines how external structure
+should be mapped to constructor fields
+and defines the policy of extra data processing.
+
+None means that item of dict or list maps to nothing.
+
+This structure is named in honor of the crown of the tree.
+
+For example,
+DictCrown(
+    {
+        'a': FieldCrown('x'),
+        'b': ListCrown(
+            {
+                0: FieldCrown('y'),
+                1: FieldCrown('z'),
+                2: None,
+            },
+            extra=ExtraForbid(),
+        ),
+    },
+    extra=ExtraCollect(),
+)
+means that:
+    x = data['a']
+    y = data['b'][0]
+    z = data['b'][1]
+
+    List at data['b'] can have only 3 elements
+    and value of element with index 2 is ignored.
+
+    Dict `data` can contain additional keys
+    that will be collected and passed to constructor
+    according to FigureExtra of Figure.
+
+Gaps at ListCrown.map is filled with None, e.g.
+
+ListCrown(
+  {
+      0: FieldCrown('a'),
+      2: FieldCrown('b')
+   },
+   extra=...
+)
+
+is same as
+
+ListCrown(
+  {
+      0: FieldCrown('a'),
+      1: None,
+      2: FieldCrown('b')
+   },
+   extra=...
+)
+"""
+
 from abc import abstractmethod, ABC
 from dataclasses import dataclass
 from enum import Enum
@@ -137,11 +196,6 @@ class FieldCrown:
     name: str
 
 
-# Crown defines how external structure
-# should be mapped to constructor fields
-# and defines the policy of extra data processing.
-# None means that item of dict or list maps to nothing.
-# This structure is named in honor of the crown of the tree
 Crown = Union[FieldCrown, None, DictCrown, ListCrown]
 
 RootCrown = Union[DictCrown, ListCrown]
