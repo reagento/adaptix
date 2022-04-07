d015823b5071a9b76adb4aea6402663a910e85c6
Tishka17 <tishka17@mail.ru>
2022-04-07 13:30:40 +0300

    inline schemas, support enum

--- dataclass_factory/jsonschema.py
+++ dataclass_factory/jsonschema.py
@@ -7,7 +7,7 @@ from .fields import get_dataclass_fields, get_typeddict_fields
 from .schema import Schema, Unknown
 from .type_detection import (
     hasargs, is_iterable, is_dict, is_enum, is_generic_concrete,
-    is_none, is_tuple, is_typeddict, is_union,
+    is_none, is_tuple, is_typeddict, is_union, is_literal,
 )
 
 
@@ -16,6 +16,12 @@ def need_ref(cls) -> bool:
         return False
     if is_none(cls):
         return False
+    if is_literal(cls):
+        return False
+    if is_union(cls):
+        return False
+    if is_iterable(cls):
+        return is_typeddict(cls)
     return True
 
 
@@ -39,25 +45,32 @@ def get_type(cls) -> Optional[str]:
     return "object"
 
 
-def type_or_ref(class_, factory: AbstractFactory) -> Dict[str, Any]:
+def type_or_ref(
+    class_, factory: AbstractFactory, json_schema_definitions_path: str,
+) -> Dict[str, Any]:
     if need_ref(class_):
         ref = factory.json_schema_ref_name(class_)
-        return {"$ref": f"#/definitions/{ref}"}
-    return {"type": get_type(class_)}
+        return {"$ref": f"#{json_schema_definitions_path}/{ref}"}
+    return factory.json_schema(class_)
 
 
-def unknown(factory: AbstractFactory, schema: Schema, cls: Type) -> Dict[str, Any]:
+def unknown(
+    factory: AbstractFactory, schema: Schema, cls: Type,
+) -> Dict[str, Any]:
     res: Dict[str, Any] = {}
     if schema.unknown == Unknown.FORBID:
         res["additionalProperties"] = False
     elif schema.unknown in (Unknown.SKIP, Unknown.STORE):
-        res["additionalProperties"] = True
+        pass
     else:
-        raise NotImplementedError("Cannot generate schema with unknown=%s" % schema.unknown)
+        raise NotImplementedError(f"Cannot generate schema with unknown={schema.unknown}")
     return res
 
 
-def typed_dict_schema(factory: AbstractFactory, schema: Schema, cls: Type) -> Dict[str, Any]:
+def typed_dict_schema(
+    factory: AbstractFactory, schema: Schema, cls: Type,
+    json_schema_definitions_path: str,
+) -> Dict[str, Any]:
     res: Dict[str, Any] = {}
     fields = get_typeddict_fields(schema, cls)
     if schema.name_mapping and any(isinstance(key, tuple) for key in schema.name_mapping.values()):
@@ -65,7 +78,9 @@ def typed_dict_schema(factory: AbstractFactory, schema: Schema, cls: Type) -> Di
     res["properties"] = {}
     res.update(unknown(factory, schema, cls))
     for f in fields:
-        res["properties"][f.data_name] = type_or_ref(f.type, factory)
+        res["properties"][f.data_name] = type_or_ref(
+            f.type, factory, json_schema_definitions_path,
+        )
         if f.default is not MISSING:
             res["properties"][f.data_name]["default"] = f.default
     if cls.__total__:
@@ -75,7 +90,10 @@ def typed_dict_schema(factory: AbstractFactory, schema: Schema, cls: Type) -> Di
     return res
 
 
-def dataclass_schema(factory: AbstractFactory, schema: Schema, cls: Type) -> Dict[str, Any]:
+def dataclass_schema(
+    factory: AbstractFactory, schema: Schema, cls: Type,
+    json_schema_definitions_path: str,
+) -> Dict[str, Any]:
     res: Dict[str, Any] = {}
     fields = get_dataclass_fields(schema, cls)
     if schema.name_mapping and any(isinstance(key, tuple) for key in schema.name_mapping.values()):
@@ -83,7 +101,9 @@ def dataclass_schema(factory: AbstractFactory, schema: Schema, cls: Type) -> Dic
     res["properties"] = {}
     res.update(unknown(factory, schema, cls))
     for f in fields:
-        res["properties"][f.data_name] = type_or_ref(f.type, factory)
+        res["properties"][f.data_name] = type_or_ref(
+            f.type, factory, json_schema_definitions_path,
+        )
         if f.default is not MISSING and f.type:
             res["properties"][f.data_name]["default"] = factory.serializer(f.type)(f.default)
     res["required"] = [
@@ -92,7 +112,10 @@ def dataclass_schema(factory: AbstractFactory, schema: Schema, cls: Type) -> Dic
     return res
 
 
-def create_schema(factory: AbstractFactory, schema: Schema, cls: Type) -> Dict[str, Any]:  # noqa C901,CCR001
+def create_schema(
+    factory: AbstractFactory, schema: Schema, cls: Type,
+    json_schema_definitions_path: str,
+) -> Dict[str, Any]:  # noqa C901,CCR001
     if cls is Any:
         return {}
 
@@ -105,22 +128,40 @@ def create_schema(factory: AbstractFactory, schema: Schema, cls: Type) -> Dict[s
     if type_:
         res["type"] = type_
 
-    if is_enum(cls):
+    if cls in (str, ):
+        pass
+    elif cls in (int, float, complex, bool):
+        pass
+    elif is_enum(cls):
         res["enum"] = [x.value for x in cls]
     elif is_dict(cls):
-        res["additionalProperties"] = type_or_ref(cls.__args__[1], factory)
+        res["additionalProperties"] = type_or_ref(
+            cls.__args__[1], factory, json_schema_definitions_path,
+        )
     elif is_tuple(cls):
         if hasargs(cls):
             if len(cls.__args__) == 2 and cls.__args__[1] is Ellipsis:
-                res["items"] = type_or_ref(cls.__args__[0], factory)
+                res["items"] = type_or_ref(
+                    cls.__args__[0], factory, json_schema_definitions_path,
+                )
             else:
-                res["items"] = [type_or_ref(x, factory) for x in cls.__args__]
+                res["items"] = [
+                    type_or_ref(x, factory, json_schema_definitions_path)
+                    for x in cls.__args__
+                ]
     elif is_typeddict(cls) or (is_generic_concrete(cls) and is_typeddict(cls.__origin__)):
-        res.update(typed_dict_schema(factory, schema, cls))
+        res.update(typed_dict_schema(factory, schema, cls, json_schema_definitions_path))
     elif is_iterable(cls):
-        res["items"] = type_or_ref(cls.__args__[0], factory)
+        res["items"] = type_or_ref(
+            cls.__args__[0], factory, json_schema_definitions_path,
+        )
     elif is_union(cls):
-        res["anyOf"] = [type_or_ref(x, factory) for x in cls.__args__]
+        res["anyOf"] = [
+            type_or_ref(x, factory, json_schema_definitions_path)
+            for x in cls.__args__
+        ]
+    elif is_literal(cls):
+        res["enum"] = list(factory.serializer(type(x))(x) for x in cls.__args__)
     elif is_dataclass(cls) or (is_generic_concrete(cls) and is_dataclass(cls.__origin__)):
-        res.update(dataclass_schema(factory, schema, cls))
+        res.update(dataclass_schema(factory, schema, cls, json_schema_definitions_path))
     return res
