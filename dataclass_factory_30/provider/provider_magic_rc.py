bf8d8d69ff552d8eb062d71a3fa7cc014df5405a
Tishka17 <tishka17@mail.ru>
2022-08-14 18:45:51 +0300

    Remove exact classes

--- dataclass_factory_30/provider/provider_magic_rc.py
+++ dataclass_factory_30/provider/provider_magic_rc.py
@@ -1,14 +1,13 @@
 import re
-from typing import Type, Sequence, Union
+from typing import Sequence, Union
 
 from dataclass_factory_30.common import TypeHint
 from dataclass_factory_30.provider.provider_basics import (
-    StackEndRC, RequestChecker, ExactTypeRC,
-    OrRequestChecker, ReFieldNameRC, ExactFieldNameRC,
+    StackEndRC, RequestChecker, OrRequestChecker, ReFieldNameRC,
+    ExactFieldNameRC,
     create_type_hint_req_checker, AndRequestChecker, XorRequestChecker,
     NegRequestChecker
 )
-from dataclass_factory_30.type_tools import normalize_type
 
 RequestCheckerSrc = Union[TypeHint, str, RequestChecker, 'F']
 
@@ -37,14 +36,11 @@ class F:
     def __init__(
         self,
         *preds: Union[TypeHint, str, RequestChecker],
-        exact_classes: Sequence[Type] = (),
         stack: Sequence[RequestChecker] = (),
     ):
         current_checkers = []
         for pred in preds:
             current_checkers.append(create_req_checker(pred))
-        for cls in exact_classes:
-            current_checkers.append(ExactTypeRC(normalize_type(cls)))
         self.stack = list(stack)
         self.stack.append(OrRequestChecker(current_checkers))
 
@@ -54,9 +50,8 @@ class F:
     def __call__(
         self,
         *superclasses: type,
-        exact_classes: Sequence[Type] = (),
     ):
-        return F(*superclasses, exact_classes=exact_classes, stack=self.stack)
+        return F(*superclasses, stack=self.stack)
 
     def __getitem__(self, item: str):
         checker = ReFieldNameRC(re.compile(item))
