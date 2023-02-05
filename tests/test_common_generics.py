1f46f54ebb037318e0721d4439055afc79381f2d
Eclips4 <kirill.bast9@mail.ru>
2023-02-05 16:37:49 +0200

    Add skip for 	est_generic_unspecified_args if python version < 3.9

--- tests/test_common_generics.py
+++ tests/test_common_generics.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from unittest import TestCase
+import sys
+from unittest import skipUnless, TestCase
 
 from dataclass_factory import Factory
 
@@ -7,17 +8,6 @@ from dataclass_factory import Factory
 @dataclass
 class A:
     value: int
-
-
-@dataclass
-class Book:
-    price: int
-
-
-@dataclass
-class Bookshelf:
-    magazines: list
-    books: list[Book]
 
 
 class Test1(TestCase):
@@ -29,21 +19,32 @@ class Test1(TestCase):
         factory = Factory()
         self.assertEqual(factory.dump({"a": A(1)}), {"a": {"value": 1}})
 
+    @skipUnless(sys.version_info[:2] >= (3, 9), "requires Python 3.9+")
     def test_generic_unspecified_args(self):
         # see https://github.com/reagento/dataclass-factory/issues/207 for
         # more information
+
+        @dataclass
+        class Book:
+            price: int
+
+        @dataclass
+        class Bookshelf:
+            magazines: list
+            books: list[Book]
+
         factory = Factory()
         data = {
             "magazines": [],
             "books": [{
-                "price": 100
-            }]
+                "price": 100,
+            }],
         }
         loaded = factory.load(data, Bookshelf)
         self.assertEqual(loaded, Bookshelf(
             magazines=[],
             books=[
-                Book(price=100)
-            ]
+                Book(price=100),
+            ],
         ))
         self.assertEqual(factory.dump(loaded), data)
