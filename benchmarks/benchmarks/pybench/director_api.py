f4daa90db4959c504b37cae1868dbaf83cf6844a
pavel <p917910@gmail.com>
2023-04-23 16:09:19 +0300

    add --unstable param to run command add own more strict benchmark stability checks

--- benchmarks/benchmarks/pybench/director_api.py
+++ benchmarks/benchmarks/pybench/director_api.py
@@ -37,6 +37,11 @@ class PlotParams:
     trim_after: Optional[float] = None
 
 
+@dataclass
+class CheckParams:
+    stddev_rel_threshold: float = 0.05
+
+
 class BenchAccessor:
     def __init__(self, data_dir: Path, env_spec: Mapping[str, str], schemas: List[BenchSchema]):
         self.data_dir = data_dir
@@ -84,6 +89,176 @@ class BenchAccessor:
             tags_str = ', '.join(schema.tags)
             return f"{schema.base}\n({tags_str})"
         return schema.base
+
+
+class BenchChecker:
+    def __init__(self, check_params: CheckParams, accessor: BenchAccessor):
+        self.check_params = check_params
+        self.accessor = accessor
+
+    def add_arguments(self, parser: ArgumentParser) -> None:
+        pass
+
+    def _process_pyperf_warnings(
+        self,
+        schema: BenchSchema,
+        bench: pyperf.Benchmark,
+        warnings: Iterable[str],
+    ) -> Iterable[str]:
+        return [
+            line
+            for line in warnings
+            if not line.startswith('Use')
+        ]
+
+    def get_warnings(self, schema: BenchSchema) -> Optional[Sequence[str]]:
+        result_file_path = self.accessor.bench_result_file(self.accessor.get_id(schema))
+        if not result_file_path.exists():
+            return None
+
+        bench = pyperf.Benchmark.load(str(result_file_path))
+        warnings = self._process_pyperf_warnings(schema, bench, format_checks(bench))
+        self_warnings = self._check_yourself(schema, bench)
+        return [*warnings, *self_warnings]
+
+    def _check_yourself(self, schema: BenchSchema, bench: pyperf.Benchmark) -> Sequence[str]:
+        lines: List[str] = []
+        stdev = bench.stdev()
+        mean = bench.mean()
+        percent = stdev / mean
+        if percent >= self.check_params.stddev_rel_threshold:
+            lines.append(
+                "the relative standard deviation is {:.1%}, max allowed is {:.0%}"
+                .format(
+                    percent, self.check_params.stddev_rel_threshold
+                )
+            )
+        return lines
+
+    def check_results(self):
+        lines = []
+        for schema in self.accessor.schemas:
+            warnings = self.get_warnings(schema)
+            if warnings is None:
+                lines.append(f'Result file of {self.accessor.get_id(schema)!r}')
+                lines.append('')
+            elif warnings:
+                lines.append(self.accessor.get_id(schema))
+                lines.extend(warnings)
+
+        print('\n'.join(lines))
+
+
+class BenchRunner:
+    def __init__(self, accessor: BenchAccessor, checker: BenchChecker):
+        self.accessor = accessor
+        self.checker = checker
+
+    def add_arguments(self, parser: ArgumentParser) -> None:
+        selective_group = parser.add_mutually_exclusive_group()
+        selective_group.add_argument('--include', '-i', action='extend', nargs="+", required=False)
+        selective_group.add_argument('--exclude', '-e', action='extend', nargs="+", required=False)
+
+        scope_group = parser.add_mutually_exclusive_group()
+        scope_group.add_argument(
+            '--missing', action='store_true', required=False, default=False,
+            help='run only missing benchmarks'
+        )
+        scope_group.add_argument(
+            '--unstable', action='store_true', required=False, default=False,
+            help='run only unstable or missing benchmarks'
+        )
+
+    def run_benchmarks(
+        self,
+        include: Optional[Sequence[str]] = None,
+        exclude: Optional[Sequence[str]] = None,
+        missing: bool = False,
+        unstable: bool = False,
+    ) -> None:
+        if missing:
+            schemas = [
+                schema for schema in self.accessor.schemas
+                if not self.accessor.bench_result_file(self.accessor.get_id(schema)).exists()
+            ]
+        elif unstable:
+            schemas = [
+                schema for schema, warnings in (
+                    (schema, self.checker.get_warnings(schema))
+                    for schema in self.accessor.schemas
+                )
+                if warnings is None or warnings
+            ]
+        else:
+            schemas = self.accessor.schemas
+
+        local_id_to_schema = {
+            self.accessor.get_local_id(schema): schema
+            for schema in schemas
+        }
+
+        benchmarks_to_run: List[str]
+        if exclude is not None:
+            benchmarks_to_run = [
+                self.accessor.get_local_id(schema)
+                for schema in schemas
+                if self.accessor.get_local_id(schema) not in set(exclude)
+            ]
+        elif include is not None:
+            wild_local_ids = set(include) - local_id_to_schema.keys()
+            if wild_local_ids:
+                raise ValueError(f"Unknown local ids {wild_local_ids}")
+            benchmarks_to_run = list(include)
+        else:
+            benchmarks_to_run = [self.accessor.get_local_id(schema) for schema in schemas]
+
+        print('Benchmarks to run: ' + " ".join(benchmarks_to_run))
+        for tag in benchmarks_to_run:
+            self.run_one_benchmark(local_id_to_schema[tag])
+
+    def run_one_benchmark(self, schema: BenchSchema) -> None:
+        sig = inspect.signature(schema.func)
+        bench_id = self.accessor.get_id(schema)
+        result_file = self.accessor.bench_result_file(bench_id)
+        with TemporaryDirectory() as dir_name:
+            temp_file = Path(dir_name) / f"{bench_id}.json"
+            print(f'start: {bench_id}')
+            self.launch_benchmark(
+                bench_name=bench_id,
+                entrypoint=get_function_object_ref(schema.func),
+                params=[schema.kwargs[param] for param in sig.parameters.keys()],
+                extra_args=['-o', str(temp_file)]
+            )
+            result_file.write_text(
+                json.dumps(
+                    json.loads(temp_file.read_text()),
+                    ensure_ascii=False,
+                    indent=4,
+                )
+            )
+            shutil.move(temp_file, result_file)
+
+    def launch_benchmark(
+        self,
+        bench_name: str,
+        entrypoint: str,
+        params: List[Any],
+        extra_args: Iterable[str] = (),
+    ) -> None:
+        subprocess.run(
+            [
+                sys.executable, '-m', 'benchmarks.pybench.pyperf_runner',
+                '--inherit-environ', 'PYBENCH_NAME,PYBENCH_ENTRYPOINT,PYBENCH_PARAMS',
+                *extra_args,
+            ],
+            env={
+                **os.environ,
+                'PYBENCH_NAME': bench_name,
+                'PYBENCH_ENTRYPOINT': entrypoint,
+                'PYBENCH_PARAMS': json.dumps(params),
+            },
+            check=True,
+        )
 
 
 class BenchPlotter:
@@ -157,141 +332,6 @@ class BenchPlotter:
         ax.set_axisbelow(True)
         plt.savefig(output, dpi=dpi)
 
-
-class BenchRunner:
-    def __init__(self, accessor: BenchAccessor):
-        self.accessor = accessor
-
-    def add_arguments(self, parser: ArgumentParser) -> None:
-        group = parser.add_mutually_exclusive_group()
-        group.add_argument('--include', '-i', action='extend', nargs="+", required=False)
-        group.add_argument('--exclude', '-e', action='extend', nargs="+", required=False)
-        group.add_argument(
-            '--missing', action='store_true', required=False, default=False,
-            help='run only missing benchmarks'
-        )
-
-    def run_benchmarks(
-        self,
-        include: Optional[Sequence[str]] = None,
-        exclude: Optional[Sequence[str]] = None,
-        missing: bool = False,
-    ) -> None:
-        if not missing:
-            schemas = self.accessor.schemas
-        else:
-            schemas = [
-                schema for schema in self.accessor.schemas
-                if not self.accessor.bench_result_file(self.accessor.get_id(schema)).exists()
-            ]
-        local_id_to_schema = {
-            self.accessor.get_local_id(schema): schema
-            for schema in schemas
-        }
-
-        benchmarks_to_run: List[str]
-        if exclude is not None:
-            benchmarks_to_run = [
-                self.accessor.get_local_id(schema)
-                for schema in schemas
-                if self.accessor.get_local_id(schema) not in set(exclude)
-            ]
-        elif include is not None:
-            wild_local_ids = set(include) - local_id_to_schema.keys()
-            if wild_local_ids:
-                raise ValueError(f"Unknown local ids {wild_local_ids}")
-            benchmarks_to_run = list(include)
-        else:
-            benchmarks_to_run = [self.accessor.get_local_id(schema) for schema in schemas]
-
-        for tag in benchmarks_to_run:
-            self.run_one_benchmark(local_id_to_schema[tag])
-
-    def run_one_benchmark(self, schema: BenchSchema) -> None:
-        sig = inspect.signature(schema.func)
-        bench_id = self.accessor.get_id(schema)
-        result_file = self.accessor.bench_result_file(bench_id)
-        with TemporaryDirectory() as dir_name:
-            temp_file = Path(dir_name) / f"{bench_id}.json"
-            print(f'start: {bench_id}')
-            self.launch_benchmark(
-                bench_name=bench_id,
-                entrypoint=get_function_object_ref(schema.func),
-                params=[schema.kwargs[param] for param in sig.parameters.keys()],
-                extra_args=['-o', str(temp_file)]
-            )
-            result_file.write_text(
-                json.dumps(
-                    json.loads(temp_file.read_text()),
-                    ensure_ascii=False,
-                    indent=4,
-                )
-            )
-            shutil.move(temp_file, result_file)
-
-    def launch_benchmark(
-        self,
-        bench_name: str,
-        entrypoint: str,
-        params: List[Any],
-        extra_args: Iterable[str] = (),
-    ) -> None:
-        subprocess.run(
-            [
-                sys.executable, '-m', 'benchmarks.pybench.pyperf_runner',
-                '--inherit-environ', 'PYBENCH_NAME,PYBENCH_ENTRYPOINT,PYBENCH_PARAMS',
-                *extra_args,
-            ],
-            env={
-                **os.environ,
-                'PYBENCH_NAME': bench_name,
-                'PYBENCH_ENTRYPOINT': entrypoint,
-                'PYBENCH_PARAMS': json.dumps(params),
-            },
-            check=True,
-        )
-
-
-class BenchChecker:
-    def __init__(self, accessor: BenchAccessor):
-        self.accessor = accessor
-
-    def add_arguments(self, parser: ArgumentParser) -> None:
-        pass
-
-    def _process_warnings_batch(
-        self,
-        schema: BenchSchema,
-        bench: pyperf.Benchmark,
-        warnings: Iterable[str],
-    ) -> Iterable[str]:
-        return [
-            self.accessor.get_id(schema)
-        ] + [
-            line
-            for line in warnings
-            if not line.startswith('Use')
-        ]
-
-    def check_results(self):
-        lines = []
-        for schema in self.accessor.schemas:
-            result_file_path = self.accessor.bench_result_file(self.accessor.get_id(schema))
-
-            if not result_file_path.exists():
-                lines.append(f'Result file of {self.accessor.get_id(schema)!r}')
-                lines.append('')
-                continue
-
-            bench = pyperf.Benchmark.load(str(result_file_path))
-            warnings = format_checks(bench)
-            if warnings:
-                lines.extend(self._process_warnings_batch(schema, bench, warnings))
-                lines.append('')
-
-        print('\n'.join(lines))
-
-
 def call_by_namespace(func: Callable, namespace: Namespace) -> Any:
     sig = inspect.signature(func)
     kwargs_for_func = (vars(namespace).keys() & sig.parameters.keys())
@@ -305,11 +345,13 @@ class BenchmarkDirector:
         plot_params: PlotParams,
         env_spec: Mapping[str, str],
         schemas: Iterable[BenchSchema] = (),
+        check_params: CheckParams = CheckParams(),
     ):
         self.data_dir = data_dir
         self.env_spec = env_spec
         self.plot_params = plot_params
         self.schemas: List[BenchSchema] = list(schemas)
+        self.check_params = check_params
 
     def add(self, *schemas: BenchSchema) -> None:
         self.schemas.extend(schemas)
@@ -318,9 +360,9 @@ class BenchmarkDirector:
         accessor = self.make_accessor()
         self._validate_schemas(accessor)
 
-        runner = self.make_bench_runner(accessor)
-        plotter = self.make_bench_plotter(accessor)
         checker = self.make_bench_checker(accessor)
+        runner = self.make_bench_runner(accessor, checker)
+        plotter = self.make_bench_plotter(accessor)
 
         parser = self._make_parser(accessor, runner, plotter, checker)
         namespace = parser.parse_args(args)
@@ -380,14 +422,14 @@ class BenchmarkDirector:
             schemas=self.schemas,
         )
 
-    def make_bench_runner(self, accessor: BenchAccessor) -> BenchRunner:
-        return BenchRunner(accessor)
+    def make_bench_runner(self, accessor: BenchAccessor, checker: BenchChecker) -> BenchRunner:
+        return BenchRunner(accessor, checker)
 
     def make_bench_plotter(self, accessor: BenchAccessor) -> BenchPlotter:
         return BenchPlotter(self.plot_params, accessor)
 
     def make_bench_checker(self, accessor: BenchAccessor) -> BenchChecker:
-        return BenchChecker(accessor)
+        return BenchChecker(self.check_params, accessor)
 
     def _validate_schemas(self, accessor: BenchAccessor):
         local_id_set: Set[str] = set()
