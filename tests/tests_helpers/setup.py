451e30ead33f3df44920af1ea3098f1e5d4f0f8e
pavel <p917910@gmail.com>
2023-11-26 14:49:09 +0300

    add extras attrs and attrs-strict fix outdated comment

--- tests/tests_helpers/setup.py
+++ tests/tests_helpers/setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup
 
-# cannot use pyproject.toml because it requires accessing to module as tests_helpers.tests_helpers
-
 setup(
     name='tests_helpers',
     version='0.0.0',
