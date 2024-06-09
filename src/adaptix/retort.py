660463634fd35475834798a224a498ad1a8f12aa
daler-sz <zdaler014@gmail.com>
2024-06-09 12:40:32 +0500

    rename NoSuitableProvider to ProviderNotFoundError

--- src/adaptix/retort.py
+++ src/adaptix/retort.py
@@ -1,8 +1,8 @@
 from adaptix._internal.retort.base_retort import BaseRetort
-from adaptix._internal.retort.operating_retort import NoSuitableProvider, OperatingRetort
+from adaptix._internal.retort.operating_retort import OperatingRetort, ProviderNotFoundError
 
 __all__ = (
     "BaseRetort",
-    "NoSuitableProvider",
+    "ProviderNotFoundError",
     "OperatingRetort",
 )
