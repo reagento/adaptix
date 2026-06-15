6ad46d03a1f0054865c7545f7f507b2217c2d8e2
niccolum <lastsal@mail.ru>
2026-06-15 18:11:53 +0300

    internal_modules_to_public

--- src/adaptix/provider/__init__.py
+++ src/adaptix/provider/__init__.py
@@ -1,15 +1,39 @@
-from adaptix._internal.provider.essential import AggregateCannotProvide, CannotProvide, Mediator, Provider, Request
+from adaptix._internal.model_tools.definitions import DefaultValue, InputShape, Param, ParamKind
+from adaptix._internal.morphing.model.loader_provider import ModelLoaderProvider
+from adaptix._internal.morphing.request_cls import LoaderRequest
+from adaptix._internal.provider.essential import (
+    AggregateCannotProvide,
+    CannotProvide,
+    Mediator,
+    Provider,
+    Request,
+    RequestHandlerRegisterRecord,
+)
 from adaptix._internal.provider.loc_stack_filtering import LocStackPattern, P, create_loc_stack_checker
+from adaptix._internal.provider.located_request import LocatedRequest
 from adaptix._internal.provider.provider_wrapper import Chain
+from adaptix._internal.provider.request_checkers import AlwaysTrueRequestChecker
+from adaptix._internal.provider.shape_provider import InputShapeRequest, provide_generic_resolved_shape
 
 __all__ = (
     "AggregateCannotProvide",
+    "AlwaysTrueRequestChecker",
     "CannotProvide",
     "Chain",
+    "DefaultValue",
+    "InputShape",
+    "InputShapeRequest",
+    "LoaderRequest",
     "LocStackPattern",
+    "LocatedRequest",
     "Mediator",
+    "ModelLoaderProvider",
     "P",
+    "Param",
+    "ParamKind",
     "Provider",
     "Request",
+    "RequestHandlerRegisterRecord",
     "create_loc_stack_checker",
+    "provide_generic_resolved_shape",
 )
